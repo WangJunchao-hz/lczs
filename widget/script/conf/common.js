75a0bd915fd76107026ca79236dd399fdeb0c579
zhugexiaoxiaoliang <1768368568@qq.com>
2017-07-25 19:44:12 +0800

    提交代码

--- widget/script/conf/common.js
+++ widget/script/conf/common.js
@@ -5,7 +5,7 @@
  * @Last Modified time: 2017/5/4
  * @file 常用的方法
  */
-define(["zepto","api","vue"],function($,$api,Vue) {
+define(["zepto", "api", "vue"], function ($, $api, Vue) {
     /**
      * @description: 新建一个类
      */
@@ -18,7 +18,7 @@ define(["zepto","api","vue"],function($,$api,Vue) {
     /**
      * @description: 初始化参数(全局变量+常量)
      */
-    Common.prototype.init = function() {
+    Common.prototype.init = function () {
         var self = this;
         window.$ = $;
         window.$api = $api;
@@ -32,15 +32,19 @@ define(["zepto","api","vue"],function($,$api,Vue) {
         }
         window.delay = delay;
         self.delay = delay; //页面打开延时
+
+        //判断页面是否登录
+        self.userId = $api.getStorage("userId"); //获取用户ID
     };
 
     /**
      * @description: 初始化页面(首屏)
      */
-    Common.prototype.initUI = function() {
+    Common.prototype.initUI = function () {
         var self = this;
+
         var headerDom = $api.byId("jHeader");
-        if(headerDom){
+        if (headerDom) {
             $api.fixStatusBar($api.byId("jHeader"));
         }
     };
@@ -48,11 +52,11 @@ define(["zepto","api","vue"],function($,$api,Vue) {
     /**
      * @description: 事件管理
      */
-    Common.prototype.events = function() {
+    Common.prototype.events = function () {
         var self = this;
         var hasBack = $('#jBack').length != 0 ? true : false;
-        if(hasBack){
-            $('html').on('click','#jBack',function () {
+        if (hasBack) {
+            $('html').on('click', '#jBack', function () {
                 api.closeWin();
             });
         }
@@ -61,7 +65,7 @@ define(["zepto","api","vue"],function($,$api,Vue) {
     /**
      * @description: ajax管理
      */
-    Common.prototype.https = function() {
+    Common.prototype.https = function () {
         var self = this;
         return {};
     };
@@ -69,7 +73,7 @@ define(["zepto","api","vue"],function($,$api,Vue) {
     /**
      * @description: 方法管理
      */
-    Common.prototype.methods = function() {
+    Common.prototype.methods = function () {
         var self = this;
         return {
             /**
@@ -78,7 +82,7 @@ define(["zepto","api","vue"],function($,$api,Vue) {
              * @param param[JSON] 传入参数
              * @param isBounces[Boolean] 窗口是否弹动
              */
-            openWin: function(name, url, param, isBounces) {
+            openWin: function (name, url, param, isBounces) {
                 api.openWin({
                     name: name,
                     url: url, //以当前窗口为相对路径而不是js
@@ -94,30 +98,30 @@ define(["zepto","api","vue"],function($,$api,Vue) {
              * @param param[JSON] 传入参数
              * @param isBounces[Boolean] 窗口是否弹动
              */
-            openFrame: function(name, url,position, param, isBounces) {
-            	var headEl = $api.byId("jHeader");
-            	var footEl = $api.byId("jFooter");
-            	var headerH = 0,footerH = 0;
-            	switch (position){
-            		case 0:
-            			if(headEl){
-            				headerH = $api.offset(headEl).h;
-            			}
-            			if(footEl){
-            				footerH = $api.offset(footEl).h;
-            			}
-            			break;
-            		case 1:
-            			if(footEl){
-            				footerH = $api.offset(footEl).h;
-            			}
-            			break;
-            		case 2:
-            			if(headEl){
-            				headerH = $api.offset(headEl).h;
-            			}
-            			break;
-            	}
+            openFrame: function (name, url, position, param, isBounces) {
+                var headEl = $api.byId("jHeader");
+                var footEl = $api.byId("jFooter");
+                var headerH = 0, footerH = 0;
+                switch (position) {
+                    case 0:
+                        if (headEl) {
+                            headerH = $api.offset(headEl).h;
+                        }
+                        if (footEl) {
+                            footerH = $api.offset(footEl).h;
+                        }
+                        break;
+                    case 1:
+                        if (footEl) {
+                            footerH = $api.offset(footEl).h;
+                        }
+                        break;
+                    case 2:
+                        if (headEl) {
+                            headerH = $api.offset(headEl).h;
+                        }
+                        break;
+                }
                 api.openFrame({
                     name: name,
                     url: url, //以当前窗口为相对路径而不是js
@@ -130,6 +134,90 @@ define(["zepto","api","vue"],function($,$api,Vue) {
                     bounces: isBounces || false,
                     pageParam: param || '',
                     delay: self.delay
+                });
+            },
+            readFile: function (name) { // 读取数据
+                try {
+                    var path = name.indexOf('widget') != -1 ? name : ('fs://data/' + name + '.json');
+                    var data = api.readFile({
+                        sync: true,
+                        path: path
+                    });
+                    return JSON.parse(data);
+                }catch (e){
+                    self.methods().showMsg("读取数据失败");
+                }
+            },
+            queryData: function (name,id) { //查询数据
+                try{
+                    var all = self.methods().readFile(name);
+                    var result;
+                    $.each(all.contents,function (i,obj) {
+                        if(obj.id == id){
+                            result = obj;
+                            return false;
+                        }
+                    });
+                    return result;
+                }catch (e){
+                    self.methods().showMsg("查询数据失败");
+                }
+            },
+            writeFile: function (name, param, tip) { // 本地存储数据
+                try {
+                    var json = {"contents":[]},newData;
+                    var oldData = self.methods().readFile(name);
+                    if(oldData && (typeof oldData == 'object')){
+                        oldData.contents.unshift(param);
+                        newData = JSON.stringify(oldData);
+                    }else {
+                        json.contents.unshift(param);
+                        newData = JSON.stringify(json);
+                    }
+                    api.writeFile({
+                        path: 'fs://data/' + name + '.json',
+                        data: newData
+                    }, function (ret, err) {
+                        if (ret.status) {
+                            self.methods().showMsg(tip);
+                        } else {
+                            self.methods().showMsg(JSON.stringify(err));
+                        }
+                    });
+                }catch (e){
+                    self.methods().showMsg("写入数据失败");
+                }
+            },
+            toThousands: function (num) { //金额转化千位分隔符
+                var val = parseFloat(num || 0).toFixed(2) + '';
+                var a = val.split('.');
+                var b = (a[0] || 0).toString().replace(/(\d)(?=(?:\d{3})+$)/g, '$1,');
+                return b + '.' + a[1];
+            },
+            uuid: function (len, radix) { //生成唯一标识 len长度 radix进制
+                var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+                var uuid = [], i;
+                radix = radix || chars.length;
+                if (len) {
+                    for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random() * radix];
+                } else {
+                    var r;
+                    uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+                    uuid[14] = '4';
+                    for (i = 0; i < 36; i++) {
+                        if (!uuid[i]) {
+                            r = 0 | Math.random() * 16;
+                            uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
+                        }
+                    }
+                }
+                return uuid.join('');
+            },
+            showMsg:function (msg) { //弹出提示信息
+                api.toast({
+                    msg: msg,
+                    duration: 2000,
+                    location: 'middle'
                 });
             }
         };
